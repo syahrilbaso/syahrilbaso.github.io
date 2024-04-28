81eaac89ff82ebfb9c68f0aed3ff8dc71e2ef950
Fireflies Developers <99664845+syahrilbaso@users.noreply.github.com>
2024-04-27 22:04:17 -0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,6 +47,7 @@ function scrollTop(){
     // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
     if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
 }
+window.addEventListener('scroll', scrollTop)
 
 /*===== SCROLL REVEAL ANIMATION =====*/
 const sr = ScrollReveal({
