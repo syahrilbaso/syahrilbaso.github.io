757a1a59feebb8c02740f1e074399386cc5fd801
Fireflies Developers <99664845+syahrilbaso@users.noreply.github.com>
2024-11-09 02:33:49 -0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,6 +63,37 @@ sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img, .nav__toggl
 sr.reveal('.home__social-icon',{ interval: 1000}); 
 sr.reveal('.skills__data, .work__img, .contact__input, .container, .card_body, .card_img, .card_title',{interval: 1000}); 
 
+// Javascipt Zona Waktu
+
+function showTime(){
+	var date = new Date();
+	var h = date.getHours();
+	var m = date.getMinutes();
+	var s = date.getSeconds();
+	var session = "AM";
+
+	if(h == 0){
+		h = 12;
+	}
+	if (h > 12) {
+		h = h - 12 ;
+		session = "PM";
+	}
+
+	h = (h<10) ? "0" + h : h;
+	m = (m<10) ? "0" + m : m;
+	s = (s<10) ? "0" + s : s;
+
+	var time = h + ":" + m + ":" + s + " " + session;
+
+	document.getElementById("DisplayClock").innerText = time;
+	document.getElementById("DisplayClock").textContent = time;
+
+	setTimeout(showTime, 1000);
+}
+
+showTime();
+
 // Untuk loading screen//
 $(document).ready(function(){
     setTimeout(() => {
